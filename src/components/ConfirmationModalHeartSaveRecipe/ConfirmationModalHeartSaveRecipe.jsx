821a7d19f5aa711059b0317c3915ee1972f0389a
angelique_rosin <angie1979.capucine@yahoo.fr>
2023-07-04 09:50:06 +0200

    add modale confirmation save recipe

--- src/components/ConfirmationModalHeartSaveRecipe/ConfirmationModalHeartSaveRecipe.jsx
+++ src/components/ConfirmationModalHeartSaveRecipe/ConfirmationModalHeartSaveRecipe.jsx
@@ -1,17 +1,24 @@
+import { Link } from "react-router-dom";
 import {
   ModalWrapper,
   ModalContent,
   Button,
-  ModalTitle,
+  // ModalTitle,
+  CloseButton,
+  StyledP,
 } from "./ConfirmationModalHeartSaveRecipe.styled";
 
 const ConfirmationModalHeartSaveRecipe = ({ onClose }) => {
   return (
     <ModalWrapper>
       <ModalContent>
-        <ModalTitle>Recette enregistrée!</ModalTitle>
-        <p>Votre recette a été ajoutée à votre carnet de recettes.</p>
-        <Button onClick={onClose}>OK</Button>
+        <CloseButton onClick={onClose}>&times;</CloseButton>
+        <StyledP>
+          Votre recette a été ajoutée à votre carnet de recettes.
+        </StyledP>
+        <Link to="/mes-recettes">
+          <Button>Voir la recette dans mon carnet</Button>
+        </Link>
       </ModalContent>
     </ModalWrapper>
   );
--- src/components/ConfirmationModalHeartSaveRecipe/ConfirmationModalHeartSaveRecipe.styled.js
+++ src/components/ConfirmationModalHeartSaveRecipe/ConfirmationModalHeartSaveRecipe.styled.js
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+0% { opacity: 0; }
+100% { opacity: 1; }
+`;
 
 export const ModalWrapper = styled.div`
   position: fixed;
@@ -11,28 +17,45 @@ export const ModalWrapper = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 1000;
+  animation: ${fadeIn} 1s ease-in-out;
 `;
 
 export const ModalContent = styled.div`
   width: 300px;
+  height: 180px;
   padding: 20px;
   background: white;
   border-radius: 8px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
   text-align: center;
+  position: relative;
 `;
 
 export const Button = styled.button`
-  margin-top: 20px;
+  margin-top: 42px;
   padding: 10px 20px;
   background-color: rgb(255, 66, 105);
   color: white;
   border: none;
-  border-radius: 4px;
+  border-radius: 25px;
   cursor: pointer;
   font-weight: bold;
 `;
 
-export const ModalTitle = styled.h3`
-  padding-bottom: 10px;
+export const CloseButton = styled.button`
+  position: absolute;
+  top: -3px;
+  right: 5px;
+  background: none;
+  border: none;
+  font-size: 2.5rem;
+  cursor: pointer;
+  // color: rgb(255, 66, 105);
+  &:hover {
+    color: rgb(255, 66, 105);
+  }
+`;
+
+export const StyledP = styled.p`
+  margin-top: 20px;
 `;
